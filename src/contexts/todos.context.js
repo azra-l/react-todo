dae3a1eb5d4d1e4aa58a997b0bea1ab7c1742f18
Azra Lalji <azralalji@Azras-MacBook-Air.local>
2021-12-28 19:02:31 -0800

    reducer for local storage

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -1,11 +1,12 @@
 import React, { createContext, useReducer } from "react";
 import reducer from "../reducers/todo.reducer";
+import useLocalStorageReducer from "../reducers/useLocalStorage.reducer";
 
 export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export const TodosProvider = (props) => {
-  const [todos, dispatch] =  useReducer(reducer, []);
+  const [todos, dispatch] =  useLocalStorageReducer("todos", [], reducer);
 
    return(
        <TodosContext.Provider value={todos}>
